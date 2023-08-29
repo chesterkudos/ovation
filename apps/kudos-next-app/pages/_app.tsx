78dc2d3e58d29115cf387c95dcde3121866d1525
Chester Rivas <chester.rivas@kudos.com>
2023-08-28 20:46:08 -0400

    ARC-164: adding new login route and styling

--- apps/kudos-next-app/pages/_app.tsx
+++ apps/kudos-next-app/pages/_app.tsx
@@ -1,18 +1,20 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "./styles.css";
+import { H2 } from "@kudos/kudos-styled";
+import Link from "next/link";
 
-function CustomApp({ Component, pageProps }: AppProps) {
+const AppIndex = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>Welcome to kudos-next-app!</title>
+        <title>Kudos Homepage</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
     </>
   );
-}
+};
 
-export default CustomApp;
+export default AppIndex;
--- libs/kudos-styled/src/lib/kudos-styled.tsx
+++ libs/kudos-styled/src/lib/kudos-styled.tsx
@@ -1,8 +1,19 @@
 import styled from "@emotion/styled";
 import { SizeMap, SIZES, Sizes } from "@kudos/kudos-types";
 
-export const Text = styled.p<{ size?: Sizes; color?: string }>`
+type TextProps = { size?: Sizes; color?: string };
+
+export const Text = styled.p<TextProps>`
   color: ${({ color = "#000" }) => color};
   font-size: ${({ size = SIZES.M }) => SizeMap[size]};
-  font-weight: normal;
+`;
+
+export const H1 = styled.h1<TextProps>`
+  color: ${({ color = "#000" }) => color};
+  font-size: ${({ size = SIZES.M }) => SizeMap[size]};
+`;
+
+export const H2 = styled.h2<TextProps>`
+  color: ${({ color = "#000" }) => color};
+  font-size: ${({ size = SIZES.M }) => SizeMap[size]};
 `;
