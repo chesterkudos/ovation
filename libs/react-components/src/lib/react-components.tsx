f51b14d348a3788217f5aad36f612b332e444213
Chester Rivas <chester.rivsa@kudos.com>
2023-08-30 11:06:45 -0400

    ARC-164: creating nav example

--- libs/react-components/src/lib/react-components.tsx
+++ libs/react-components/src/lib/react-components.tsx
@@ -1,21 +1,18 @@
 import styled from "@emotion/styled";
 
-/* eslint-disable-next-line */
-export interface ReactComponentsProps {}
+export type ReactComponentsProps = {
+  children: React.ReactNode;
+};
 
-const StyledReactComponents = styled.div`
+const HeaderContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   background-color: #ea8810;
   padding: 1rem;
 `;
 
-export function ReactComponents(props: ReactComponentsProps) {
-  return (
-    <StyledReactComponents>
-      <h2>Component from React Library</h2>
-    </StyledReactComponents>
-  );
+export function Header({ children }: ReactComponentsProps) {
+  return <HeaderContainer>{children}</HeaderContainer>;
 }
 
-export default ReactComponents;
+export default Header;
