1db67bcfeade9a2bba44812a7e7a1e3a4e4c98ca
Chester Rivas <chester.rivsa@kudos.com>
2023-08-30 11:06:45 -0400

    ARC-164: adding react component library
    - importing react component into main application
    - updating README file

--- apps/web-app/src/app/app.tsx
+++ apps/web-app/src/app/app.tsx
@@ -1,16 +1,22 @@
 import styled from "@emotion/styled";
 
 import NxWelcome from "./nx-welcome";
+import { ReactComponents } from "@kudos/react-components";
 
-const StyledApp = styled.div`
-  // Your style here
+const MainAppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  background-color: #cedcd1;
+  align-content: stretch;
 `;
 
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="web-app" />
-    </StyledApp>
+    <MainAppContainer>
+      <ReactComponents /> {/* could be a header component */}
+      <NxWelcome title="kudos web-app" />
+    </MainAppContainer>
   );
 }
 
